c864cd190c3a250ba96b8b6d19b409989fb4c296
Filipe Azevedo <6899567+madfa@users.noreply.github.com>
2022-11-16 20:50:01 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/Hashicorp-workshop-netauto-terraform/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "${var.prefix}"
   bucket = "Gaurav"
   acl    = "private"
 
